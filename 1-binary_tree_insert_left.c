db9d55511aae502627747d22953e3f2d5d99a64c
ChongLeangUENG <leangcnrp777@gmail.com>
2022-08-29 01:08:23 -0700

    Commit function that inserts a node as the left-child of another node

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function that inserts a node as the left-child
+ * @parent: Node parent
+ * @value: Node value
+ * Return: pointer to the created node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->parent = parent;
+	newnode->n = value;
+	newnode->left = parent->left;
+	newnode->right = NULL;
+
+	if (parent->left != NULL)
+		parent->left->parent = newnode;
+
+	parent->left = newnode;
+
+	return (newnode);
+}
