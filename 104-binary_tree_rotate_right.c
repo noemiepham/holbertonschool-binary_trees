ef71d05c96b8faf28a51820745b1a2bbf17115ec
ChongLeangUENG <leangcnrp777@gmail.com>
2022-08-30 07:49:39 -0700

    perform a right-rotation on a binary tree

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -7,18 +7,23 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
+	binary_tree_t *tmp;
+	
 	if (tree == NULL || tree->left == NULL)
 		return (NULL);
 
 	tree->parent = tree->left;
-	if (tree->left->right != NULL)
+	tmp = tree->left->right;
+
+	if (tmp != NULL)
 	{
 		tree->left = tree->parent->right;
+		tmp->parent = tree;
 	}
 	else
 		tree->left = NULL;
 	tree->parent->right = tree;
-
 	tree = tree->parent;
+
 	return (tree);
 }
