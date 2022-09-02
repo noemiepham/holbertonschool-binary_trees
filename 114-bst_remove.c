5747ee03f060eb7a1c0dd2faf1383aa6a5eb8029
ChongLeangUENG <leangcnrp777@gmail.com>
2022-09-02 13:03:50 -0700

    Remove BST

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -2,7 +2,7 @@
 
 /**
  * bst_remove - removes a node from a Binary Search Tree
- * @root: pointer to the root node of the tree 
+ * @root: pointer to the root node of the tree
  * @value: value to remove in the tree
  * Return: pointer to the new root node of the tree
  */
@@ -12,22 +12,19 @@ bst_t *bst_remove(bst_t *root, int value)
 
 	if (root == NULL)
 		return (root);
-	
 	if (value > root->n)
 		root->right = bst_remove(root->right, value);
 	else if (value < root->n)
 		root->left = bst_remove(root->left, value);
 	else
 	{
-		if (root->left == NULL && root->right == NULL)
-		{
-			free(root);
-			root = NULL;
-		}
+		if (!root->left && !root->right)
+		{	free(root);
+			root = NULL; }
 		else if (root->left && root->right)
 		{
 			next_root = root->right;
-			while (next_root->right)
+			while (nexit_root->left)
 				next_root = next_root->left;
 			root->n = next_root->n;
 			root->right = bst_remove(root->right, next_root->n);
