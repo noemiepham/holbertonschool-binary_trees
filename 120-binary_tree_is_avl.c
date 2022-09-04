ae679e0cd78bea655f5169230c9db6acc0972254
ChongLeangUENG <leangcnrp777@gmail.com>
2022-09-04 12:58:29 -0700

    Check binary tree is AVL

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int bst = binary_tree_is_bst(tree);
+	int bf = binary_tree_balance(tree);
+
+	if (tree == NULL)
+		return (0);
+
+	if (bst == 1 && (bf == 1 || bf == 0 || bf == -1))
+	{
+		if (!tree->right && !tree->left)
+			return (1);
+
+		else if (tree->left && !tree->right)
+			return (binary_tree_is_avl(tree->left));
+
+		else if (tree->right && !tree->left)
+			return (binary_tree_is_avl(tree->right));
+
+		else
+		{
+			return (binary_tree_is_avl(tree->left) &&
+				binary_tree_is_avl(tree->right));
+		}
+	}
+	return (0);
+}
